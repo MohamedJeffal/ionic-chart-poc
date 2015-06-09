44265ba64d0df8f4c7249f05f5ddeebd683fe3ec
Mohamed Jeffal <mh.jeffal@gmail.com>
2015-06-09 13:22:22 +0200

    Adding Charts

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['chart.js'])
 
 .controller('DashCtrl', function($scope) {})
 
@@ -18,7 +18,27 @@ angular.module('starter.controllers', [])
     enableFriends: true
   };
 })
-    .controller('ChartCtrl', function($scope) {
-        
-    });
+
+.controller('ChartCtrl', function($scope) {
+      $scope.type = 'doughnut';
+
+      $scope.dataset = {
+        doughnut: {
+          labels:["Download Sales", "In-Store Sales", "Mail-Order Sales"],
+          data: [300, 500, 100]
+        },
+        line: {
+          labels: ["January", "February", "March", "April", "May", "June", "July"],
+          series: ['Series A', 'Series B'],
+          data: [
+            [65, 59, 80, 81, 56, 55, 40],
+            [28, 48, 40, 19, 86, 27, 90]
+          ]
+        }
+      };
+
+      $scope.setType = function(type) {
+        $scope.type = type;
+      }
+})
 
