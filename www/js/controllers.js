b3685c4ac051d8d06705b9d3835cb16fda228c3f
Molinié Vincent <vincent.molinie@epita.fr>
2015-06-09 13:20:40 +0200

    Adding notif

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,6 +17,33 @@ angular.module('starter.controllers', [])
   $scope.settings = {
     enableFriends: true
   };
+
+    $ionicPlatform.ready(function() {
+        if(device.platform === "iOS") {
+            window.plugin.notification.local.promptForPermission();
+        }
+    });
+
+    $scope.notify = function() {
+        var alarmTime = new Date();
+        alarmTime.setMinutes(alarmTime.getMinutes() + 1);
+        $cordovaLocalNotification.add({
+            id: "1234",
+            date: alarmTime,
+            message: "Ouvrez les fenêtres, il y a trop de CO² dans votre chambre !",
+            title: "Danger CO²",
+            autoCancel: true,
+            sound: true
+        }).then(function () {
+            console.log("The notification has been set");
+        });
+    };
+
+    $scope.isScheduled = function() {
+        $cordovaLocalNotification.isScheduled("1234").then(function(isScheduled) {
+            alert("Notification 1234 Scheduled: " + isScheduled);
+        });
+    }
 })
     .controller('ChartCtrl', function($scope) {
 
